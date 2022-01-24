59a6781bfd7ff9a271c36b9350f59ada7d82708b
kdd9512 <hyunwoo8805@gmail.com>
2022-01-25 00:06:13 +0900

    20210124 3rd commit. SpringBoot 로 login 처리 학습.
    PasswordEncoder / BCryptPasswordEncoder 이용하여
    password 복호화 및 새 아이디 작성.
    
    http.authorizeRequests().antMatchers() 의 함수들 활용하여
    페이지의 공개상태를 설정(permitAll(), hasRole("user의 권한명")).
    http.formLogin(); 으로 인증에 문제가 발생했을 시 login 할 수 있는
    기본 페이지 함수 이용.

--- src/main/java/com/springbootsecuritybasic/config/SecurityConfig.java
+++ src/main/java/com/springbootsecuritybasic/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,11 +22,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 1) .antMatchers().permitAll() : 해당 페이지는 로그인 없이 모든 사용자에게 개방.
+        // 정확히는 로그인이 없더라도 익명의 사용자로 취급하여 페이지를 개방함.
+        // 2) .antMatchers().hasRole("user의 권한명") : 권한명과 일치하는 권한을 가진 유저만 접근가능.
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        http.formLogin(); // 3) 인증에 문제가 있을 경우 login 페이지로 이동.
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.inMemoryAuthentication().withUser("user1")
+        auth.inMemoryAuthentication().withUser("user1") // 작성할 user id.
                 .password("$2a$10$Cdvv0/zRe01SZN9U1fVXw.GMld3k8cCRTzUzYQIaqhxeWEJF6rtq6") // test 에서 1111을 인코딩한 결과.
-                .roles("USER");
-
+                .roles("USER"); // 이 아이디의 권한.
     }
+
+
 }
