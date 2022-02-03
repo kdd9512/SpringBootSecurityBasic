a46e16bba5a6809a578db69609a0558dc34f5cde
kdd9512 <hyunwoo8805@gmail.com>
2022-02-03 23:27:19 +0900

    20210203 10th commit. SpringBoot 로 login 처리 학습. PasswordEncoder / BCryptPasswordEncoder 이용. OAuth2 이용, google 계정으로 로그인 가능.
    자동로그인 구현.
    @PreAuthorize annotation 이용하여
    페이지 별 접근권한 조건 부여.
    
    API 서비스 구현중.
    
    /notes/ 경로 CRUD test.
    허용된 사용자만 접근할 수 있도록 (=서버의 부담을 줄이기 위한) filter 구현.
    (ApiCheck / ApiLogin).
    
    인증 성공 / 실패 처리.
    
    JWT 생성 및 검증.

---
+++ src/main/java/com/springbootsecuritybasic/util/JWTUtil.java
@@ -0,0 +1,60 @@
+package com.springbootsecuritybasic.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
+import io.jsonwebtoken.impl.DefaultJws;
+import lombok.extern.log4j.Log4j2;
+
+import java.nio.charset.StandardCharsets;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Log4j2
+public class JWTUtil {
+
+    private String secretKey = "kdd12345678";
+
+    private long expire = 60 * 24 * 30;
+
+    public String generateToken(String content) throws Exception {
+
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(Date.from(ZonedDateTime.now()
+                        .plusMinutes(expire).toInstant()))
+                .claim("sub", content)
+                .signWith(SignatureAlgorithm.HS256, secretKey.getBytes(StandardCharsets.UTF_8))
+                .compact();
+
+    }
+
+    public String validateAndExtract(String tokenStr) throws Exception {
+
+        String contentVal = null;
+
+        try {
+            DefaultJws defaultJws = (DefaultJws) Jwts.parser()
+                    .setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
+                    .parseClaimsJws(tokenStr);
+
+            log.info("defaultJws : " + defaultJws);
+            log.info(defaultJws.getBody().getClass());
+
+            DefaultClaims claims = (DefaultClaims) defaultJws.getBody();
+
+            log.info("=========================================================");
+
+            contentVal = claims.getSubject();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            contentVal = null;
+        }
+        return contentVal;
+
+    }
+
+}
